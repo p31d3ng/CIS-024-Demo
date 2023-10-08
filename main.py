edfd08917c5ad0d39a0ea6e794bf516e6e520c4a
Pei Deng <p31.d3ng@gmail.com>
2023-10-08 15:30:56 -0700

    add week 1 script

---
+++ main.py
@@ -0,0 +1,9 @@
+# Put down your name
+my_name = "Pei Deng"
+# Put down the course name you're working on
+course_name = "Python"
+# Current month (in integer please!)
+curr_month = 9 # change this to ..?
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now.")
+print("Hope you have a great time learing this course")
